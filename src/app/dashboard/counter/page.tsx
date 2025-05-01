92702151bf892cddc8b5ff1b17c90a7c1f3d2774
HazaFG <hazaelfg@gmail.com>
2025-05-01 18:47:42 +0000

    contador pero sin metadata, ya que no nos deja por el use client

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,9 +1,41 @@
-import React from "react";
+"use client";
 
-export default function counterPage() {
+import { Metadata } from "next";
+import { useState } from "react";
+
+// export const metadata: Metadata = {
+//   title: "Counter Page",
+//   description: "Simple contador",
+// };
+
+export default function CounterPage() {
+  const [contador, setContador] = useState(10);
+
+  //Manera facil de entender para mi xd
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
+
   return (
-    <>
-      <h1>Counter Page</h1>
-    </>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <span className="text-9xl">{contador}</span>
+
+      <div className="flex">
+        <button
+          onClick={incrementar}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transtition-all w-[100px] mr-2"
+        >
+          +1
+        </button>
+        <button
+          //Manera pro de hacerlo xd
+          onClick={() => setContador(contador - 1)}
+          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transtition-all w-[100px] mr-2"
+        >
+          -1
+        </button>
+      </div>
+    </div>
   );
 }
