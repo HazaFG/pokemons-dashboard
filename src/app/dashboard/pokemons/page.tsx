3d4535c43affd468657e79029206fbe0b6e76cc4
HazaFG <hazaelfg@gmail.com>
2025-05-02 20:48:11 +0000

    Cambio de peticion, mira este commit para obtener sabiduria

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,10 +1,20 @@
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+
 //Creamos nuestra funcion asincrona de JS, esta es la forma
-const getPokemons = async (limit = 20, offset = 0) => {
-  const data = await fetch(
+const getPokemons = async (
+  limit = 20, //Estos son parametros bro
+  offset = 0 //Este tambien
+): Promise<SimplePokemon[]> => {
+  const dataPostman: PokemonsResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon?limit=${limit}limit&offset=${offset}`
   ).then((res) => res.json());
 
-  return data;
+  const pokemons = dataPostman.results.map((pokemon) => ({
+    id: pokemon.url.split("/").at(-2)!,
+    name: pokemon.name,
+  }));
+
+  return pokemons;
 };
 
 export default async function PokemonsPage() {
