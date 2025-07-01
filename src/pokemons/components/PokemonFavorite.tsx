240ed4e1410d3fa3081a3a3d7119837695947ad2
HazaFG <hazaelfg@gmail.com>
2025-07-01 11:13:52 -0700

    componente mejorado

--- src/pokemons/components/PokemonFavorite.tsx
+++ src/pokemons/components/PokemonFavorite.tsx
@@ -3,24 +3,28 @@
 import { useAppSelector } from "@/store"
 import { IoHeartOutline, } from "react-icons/io5";
 import { PokemonGrid } from "./PokemonGrid";
+import { useState } from "react";
 
 export const PokemonFavorite = () => {
   const pokemonesFavoritos = useAppSelector(state => Object.values(state.pokemons))
+  const [pokemones, setPokemones] = useState(pokemonesFavoritos)
   console.log(pokemonesFavoritos)
-
-  if (pokemonesFavoritos.length === 0) {
-    return (
-      <div className="flex flex-col items-center justify-center h-[50vh] text-gray-600">
-        <IoHeartOutline size={100} className="mb-4" />
-        <p className="text-xl">No hay Pokémon favoritos.</p>
-        <p className="text-sm">¡Añade algunos desde la galería!</p>
-      </div>
-    )
-  }
 
   return (
     <>
-      <PokemonGrid pokemons={pokemonesFavoritos} />
+      {
+        pokemonesFavoritos.length === 0 ? <PokemonGrid pokemons={pokemones} /> : <NoFavorites />
+      }
     </>
+  )
+}
+
+export const NoFavorites = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-[50vh] text-gray-600">
+      <IoHeartOutline size={100} className="mb-4" />
+      <p className="text-xl">No hay Pokémon favoritos.</p>
+      <p className="text-sm">¡Añade algunos desde la galería!</p>
+    </div>
   )
 }
